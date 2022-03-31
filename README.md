c04c5abc84fde9295160b4aa0374be740888f538
Terhi Kilamo <terhi.kilamo@tuni.fi>
2022-03-31 10:43:36 +0300

    Course project template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
+# Programming 3 project template
 
+Make sure you have setup ssh-key for your GitLab account.
+
+Code will be published soon.
+
+Note! The .gitlab-ci.yml is offline at the moment.
+
+# You are allowed (and probably should) make changes to this file after you have started your project. :)
---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Sisu.java
@@ -0,0 +1,26 @@
+package fi.tuni.prog3.sisu;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Sisu extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        
+
+        var label = new Label("SISU");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
