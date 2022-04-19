3a761168eba82b197e47a7bc699125dcd18e797a
Lauri Kalliojärvi <lauri.kalliojarvi@tuni.fi>
2022-04-19 22:51:38 +0300

    StudyField-luokan rakenne sekä get-metodit

---
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/StudyField.java
@@ -0,0 +1,46 @@
+
+package fi.tuni.prog3.sisu;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lauri Kalliojärvi
+ */
+
+public class StudyField {
+
+    private ArrayList<Course> courses;
+    private Degree degree;
+    private String name;
+    private String id;
+    
+    public StudyField(ArrayList<Course> courses,
+            Degree degree,
+            String name,
+            String id ) {
+        
+        this.courses = courses;
+        this.degree = degree;
+        this.name = name;
+        this.id = id;
+        
+    }
+    
+    public ArrayList<Course> getCourses() {
+        return this.courses;
+    }
+    
+    public Degree getDegree() {
+        return this.degree;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getId() {
+        return this.id;
+    }
+    
+}
