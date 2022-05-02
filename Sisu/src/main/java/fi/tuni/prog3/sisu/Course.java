2cd9958d65f43cbe9edc915c818400ebd4adf8e0
Lauri Kalliojärvi <lauri.kalliojarvi@tuni.fi>
2022-05-02 17:09:24 +0300

    Kommentointia sekä koodin siistimistä

--- Sisu/src/main/java/fi/tuni/prog3/sisu/Course.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Course.java
@@ -2,16 +2,34 @@
 package fi.tuni.prog3.sisu;
 
 /**
- *
+ * Luokka, joka säilyttää kurssin ominaisuudet.
  * @author Lauri Kalliojärvi
  */
 
 public class Course implements Comparable<Course>{
-
+    
+    /**
+     * Kurssin laajuus opintopisteinä.
+     */
     private int studyPoints;
+    
+    /**
+     * Kurssin nimi.
+     */
     private String name;
+    
+    /**
+     * Kurssin tunniste.
+     */
     private String id;
     
+    /**
+     * Rakennin, joka ottaa arvot opintopisteille, nimelle sekä tunnisteelle
+     * parametreinä.
+     * @param studyPoints opintopisteet
+     * @param name kurssin nimi
+     * @param id kurssin tunniste
+     */
     public Course(int studyPoints,
             String name,
             String id ) {
@@ -22,23 +40,43 @@ public class Course implements Comparable<Course>{
         
     }
     
+    /**
+     * Palauttaa kurssin laajuuden opintopisteinä.
+     * @return opintopisteet
+     */
     public int getStudyPoints() {
         return this.studyPoints;
     }
     
+    /**
+     * Palauttaa kurssin nimen.
+     * @return nimi
+     */
     public String getName() {
         return this.name;
     }
     
+    /**
+     * Palauttaa kurssin tunnisteen.
+     * @return tunniste
+     */
     public String getId() {
         return this.id;
     }
     
+    /**
+     * Palauttaa Course-olion tilan nimen muodossa.
+     * @return kurssin nimi
+     */
     @Override
     public String toString() {
         return this.name;
     }
-
+    
+    /**
+     * Vertailumetodi, joka vertailee kurssia nimen perusteella toiseen kurssiin.
+     * @return 0, jos kurssit ovat samoja. 1, jos kurssit eivät ole samoja.
+     */
     @Override
     public int compareTo(Course o) {
         return name.compareTo(o.getName());
--- Sisu/src/main/java/fi/tuni/prog3/sisu/Degree.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/Degree.java
@@ -1,21 +1,52 @@
 
 package fi.tuni.prog3.sisu;
 
-import java.util.ArrayList;
 import java.util.TreeMap;
 
 /**
- *
+ * Luokka, joka säilyttää tutkinto-ohjelman ominaisuudet.
  * @author Lauri Kalliojärvi
  */
 public class Degree {
+    /**
+     * TreeMap opintosuunnista.
+     * TreeMapin avaimina opintosuuntien nimet.
+     */
    private TreeMap<String, Module> studyFields;
+   
+   /**
+     * TreeMap moduuleista.
+     * Avaimina moduulien nimet.
+     */
    private TreeMap<String, Module> modules;
+   
+   /**
+     * Tutkinto-ohjelman laajuus opintopisteissä.
+     */
    private int studyPoints;
+   
+   /**
+     * Tutkinto-ohjelman nimi.
+     */
    private String name;
+   
+   /**
+     * Tutkinto-ohjelman tunniste.
+     */
    private String id;
+   
+   /**
+     * Oppilaan valitsema opintosuunta.
+     */
    private Module selectedField;
    
+   /**
+     * Rakennin, joka ottaa arvot opintopisteille, nimelle 
+     * sekä tunnisteelle parametreina.
+     * @param studyPoints opintopisteet
+     * @param name nimi
+     * @param id tunniste
+     */
    public Degree(
            int studyPoints,
            String name,
@@ -27,43 +58,86 @@ public class Degree {
        this.id = id;  
    }
    
+   /**
+     * Palauttaa tutkinto-ohjelman opintosuunat.
+     * @return TreeMap opintosuunnista, opintosuuntien nimet avaimina 
+     * ja opintosuunta-oliot arvoina.
+     */
    public TreeMap<String, Module> getStudyFields() {
        return this.studyFields;
    }
    
+   /**
+     * Lisää moduulin moduulista kirjaapitävään TreeMappiin.
+     * @param module lisättävä moduuli.
+     */
    public void addModule(Module module){
        modules.put(module.getName(),module);
    }
    
+   /**
+     * Palauttaa TreeMapin tutkinto-ohjelman moduuleista.
+     * @return moduulit, moduulien nimet avaimina ja moduuli-oliot arvoina.
+     */
    public TreeMap<String, Module> getModules() {
        return this.modules;
    }
-
+   
+   /**
+     * Lisää annetun moduulin Tutkinto-ohjelmaan,
+     * lisäämällä sen moduuleista listaa pitävään TreeMappiin.
+     * @param module moduuli-olio.
+     */
    public void addStudyField(Module module) {
        studyFields.put(module.getName(), module);
    }
    
+   /**
+     * Palauttaa tutkinto-ohjelman laajuuden opintopisteinä.
+     * @return opintopisteiden määrä.
+     */
    public int getStudyPoints() {
        return this.studyPoints;
    }
    
+   /**
+     * Palauttaa tutkinto-ohjelman nimen.
+     * @return tutkinto-ohjelman nimi.
+     */
    public String getName() {
        return this.name;
    }
    
+   /**
+     * Palauttaa tutkinto-ohjelman tunnisteen.
+     * @return tunniste.
+     */
    public String getId() {
        return this.id;
    }
    
+   /**
+     * Palauttaa tutkinto-ohjelman tilan nimen muodossa.
+     * @return nimi
+     */
    @Override
     public String toString() {
         return this.name;
     }
-
+    
+    /**
+     * Asettaa annetun opintosuunnan tutkinto-ohjelman alaiseksi,
+     * lisäämällä opintosuunnan TreeMappiin, joka pitää listaa opintosuunnista.
+     * @param selectedFieldStr valittu opintosuunta.
+     */
     public void setSelectedField(String selectedFieldStr) {
         this.selectedField = this.studyFields.get(selectedFieldStr);
     }
     
+    /**
+     * Palauttaa valitun opintosuunnan.
+     * @return valittu opintosuunta.
+     */
     public Module getSelectedField() {
         return this.selectedField;
     }
